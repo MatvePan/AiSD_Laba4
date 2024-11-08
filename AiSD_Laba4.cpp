6b60f507c2e644f28b28a97842d7fb797213b722
MatvePan <159632905+MatvePan@users.noreply.github.com>
2024-11-08 13:42:18 +0300

    Add files via upload

---
+++ AiSD_Laba4.cpp
@@ -0,0 +1,36 @@
+﻿#include "iostream"
+#include "vector"
+using namespace std;
+
+void Enter(vector<int>& mass) {
+
+}
+
+void Tree(vector<int>& mass) {
+
+}
+
+int main() {
+	setlocale(LC_ALL, "RUS");
+	vector<int> monzh;
+	while (true) {
+		int mode;
+		cout << "1. Ввод элементов множества\n";
+		cout << "2. Вывод бинарного дерева\n";
+		cout << "0. Выход\n";
+		cout << "Введите число для выбора\n";
+		cin >> mode;
+		switch (mode) {
+		case 1:
+			Enter(monzh);
+			break;
+		case 2:
+			Tree(monzh);
+			break;
+		case 3:
+			break;
+		default:
+			cout << "Неверный ввод\n";
+		}
+	}
+}
